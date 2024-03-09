08fff6a6cd712a59226e78c0f3b12a78340597df
kali <kali@kali.kali>
2024-03-09 19:09:50 +0330

    feat: Check exists config file. Create config file if is not in directory. Save configs thay enter by user in config file .

--- sm.sh
+++ sm.sh
@@ -1,35 +1,60 @@
 #!/bin/bash
 
+
+
+
+
+function main(){
+if  existsConfigFile $0 ; then
+# Config file is not found
+createConfigFile
+init
+else
+# Config file is the directory
+run
+fi
+}
+
+function init(){
 echo "Start Config..."
 read -p "Enter Website/Domain name: " URL_ADD
 read -p "Enter your email: " EMAIL_ADD
 echo "\n \n \n "
 echo "Your Website/Domain is : $URL_ADD"
 echo "Your Email address is : $EMAIL_ADD"
-
-
-
-function main(){
-
+	
+saveConfigToFile $URL_ADD  $EMAIL_ADD
 }
 
-function init(){
+function run(){
+echo "run function"
 }
 
 function existsConfigFile(){
-	
+if [ ! -f sm.config ]; then
+return 0;
+else
+return 1;
+fi
 }
 
+
 function createConfigFile(){
-	if [ ! -f sm.config ]; then
-	# Config file is not found
-	touch sm.config
-	fi
+if existsConfigFile $0 ; then
+# Config file is not found
+touch sm.config
+fi
 }
 
 function saveConfigToFile(){
-	cat << EOF > sm.config
-	URL_ADDRESS: $URL_ADD
-	EMAIL_ADDRESS: $EMAIL_ADD
-	EOF
+cat << EOF > sm.config
+URL_ADDRESS: $1
+EMAIL_ADDRESS: $2
+EOF
 }
+
+
+
+main
+
+
